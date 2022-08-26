8855674995483dfcad725ed87c37987fec5ccbae
Sergei Ordin <seryogaordin@gmail.com>
2022-08-26 15:44:17 +0300

    added portfolio and price blocks

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -10,3 +10,9 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
